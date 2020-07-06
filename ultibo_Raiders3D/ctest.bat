efbdacd7324565f6aee9e453008115b76a385bc2
cejasmasmas <65371443+cejasmasmas@users.noreply.github.com>
2020-07-06 11:03:54 -0600

    Add files via upload

---
+++ ultibo_Raiders3D/ctest.bat
@@ -0,0 +1,9 @@
+arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -D__DYNAMIC_REENT__ -c raiders3d.c 
+arm-none-eabi-ar rcs libraiders3d.a raiders3d.o
+REM following copied from https://github.com/noshbar/Ultibo-PCE/blob/master/Ultibo/build_pi3.bat
+@echo off
+del kernel7.img
+del *.o
+del *.ppu
+c:\Ultibo\Core\fpc\3.1.1\bin\i386-win32\fpc -B -Tultibo -Parm -CpARMV7A -WpRPI3B @c:\Ultibo\Core\fpc\3.1.1\bin\i386-win32\RPI3.CFG -O2 -dPLATFORM_PI3 ultibo_raiders3D.lpr
+echo Done.
